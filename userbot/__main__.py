1bc9206c9dcb2ee054bd5d8791315530f25150b0
xHOPExINFINITY <78067174+xHOPExINFINTY@users.noreply.github.com>
2021-01-27 23:28:52 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -41,7 +41,7 @@ for name in files:
     with open(name) as f:
         path1 = Path(f.name)
         shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
+        load_module(shortname.replace(".py"))
 
 
 print(
