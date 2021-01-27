10fc2eb9d66c23bf0882d4bcd2052faf684fff15
xHOPExINFINITY <78067174+xHOPExINFINTY@users.noreply.github.com>
2021-01-27 18:27:59 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -10,7 +10,7 @@ from userbot.uniborgConfig import Config
 from var import Var
 
 StartTime = time.time()
-hellversion = "2.0"
+ELITEversion = "1.0"
 
 os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
